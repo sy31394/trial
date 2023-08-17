80737410d9cd87c970e6b25fe6c2712839f46ecb
Ramesh Ramachandran <ramesh.ramachandran@yahoo.com>
2023-08-17 11:32:55 -0400

    To make it work with AWS

--- src/main/java/com/learn/trial/domain/User.java
+++ src/main/java/com/learn/trial/domain/User.java
@@ -5,23 +5,27 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @Entity
+@Table(name = "USER")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
     Integer userId;
+    @Column(name = "FIRST_NAME")
     String firstName;
+    @Column(name = "LAST_NAME")
     String lastName;
+    @Column(name = "EMAIL")
     String email;
+    @Column(name = "GENDER")
     String gender;
+    @Column(name = "PROFESSION")
     String profession;
 }
